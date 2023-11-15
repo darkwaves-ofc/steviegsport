009673df44bda08d9d477a4d248df1cfed513926
GenerallyStevie <39565821+GenerallyStevie@users.noreply.github.com>
2023-11-15 10:22:37 -0500

    pushed new logos
    blah

--- assets/logos.js
+++ assets/logos.js
@@ -7,5 +7,15 @@ import image6 from "./images/logos/logo6.jpg";
 import image7 from "./images/logos/logo7.jpg";
 import image8 from "./images/logos/logo8.jpg";
 import image9 from "./images/logos/logo9.gif";
-
-export default [image1, image2, image3, image4, image5, image6, image7, image8, image9];
+import image10 from "./images/logos/logo10.gif"
+import image11 from "./images/logos/logo11.jpg"
+import image12 from "./images/logos/logo12.gif"
+import image13 from "./images/logos/logo13.png"
+import image14 from "./images/logos/logo14.gif"
+import image15 from "./images/logos/logo15.gif"
+import image16 from "./images/logos/logo16.gif"
+import image17 from "./images/logos/logo17.png"
+import image18 from "./images/logos/logo18.png"
+import image19 from "./images/logos/logo19.png"
+import image20 from "./images/logos/logo20.png"
+export default [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image15, image16, image17, image18, image19, image20];
